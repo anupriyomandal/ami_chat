8fcba3d8af46c639a40b4b20bf9f23f49eb04529
Anupriyo Mandal <anupriyo@Anupriyos-MacBook-Air.local>
2026-04-18 01:43:21 +0530

    fix: preserve drizzle return types in lazy DB proxy

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,12 @@
 import BetterSqlite3 from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 
-let _sqlite: any;
-let _db: any;
+type DbType = ReturnType<typeof drizzle>;
 
-function getDb() {
+let _sqlite: InstanceType<typeof BetterSqlite3> | undefined;
+let _db: DbType | undefined;
+
+function getDb(): DbType {
   if (!_sqlite) {
     const databasePath = process.env.DATABASE_PATH || "./openchat.db";
     _sqlite = new BetterSqlite3(databasePath);
@@ -12,11 +14,11 @@ function getDb() {
     _sqlite.pragma("foreign_keys = ON");
     _db = drizzle(_sqlite);
   }
-  return _db;
+  return _db!;
 }
 
-export const db = new Proxy({} as any, {
+export const db = new Proxy({} as DbType, {
   get(_target, prop) {
-    return getDb()[prop];
+    return (getDb() as any)[prop];
   },
 });
