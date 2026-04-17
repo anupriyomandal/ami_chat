fddb68403f9ab538ec84d76a95c5f119ad52427a
Anupriyo Mandal <anupriyo@Anupriyos-MacBook-Air.local>
2026-04-18 01:41:11 +0530

    fix: use any type to avoid esModuleInterop issue

--- db/index.ts
+++ db/index.ts
@@ -1,22 +1,22 @@
-import Database from "better-sqlite3";
+import BetterSqlite3 from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 
-let _sqlite: Database | undefined;
-let _db: ReturnType<typeof drizzle> | undefined;
+let _sqlite: any;
+let _db: any;
 
 function getDb() {
   if (!_sqlite) {
     const databasePath = process.env.DATABASE_PATH || "./openchat.db";
-    _sqlite = new Database(databasePath);
+    _sqlite = new BetterSqlite3(databasePath);
     _sqlite.pragma("journal_mode = WAL");
     _sqlite.pragma("foreign_keys = ON");
     _db = drizzle(_sqlite);
   }
-  return _db!;
+  return _db;
 }
 
-export const db = new Proxy({} as ReturnType<typeof drizzle>, {
+export const db = new Proxy({} as any, {
   get(_target, prop) {
-    return getDb()[prop as keyof ReturnType<typeof drizzle>];
+    return getDb()[prop];
   },
 });
